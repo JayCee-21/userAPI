1abb7ea0acea04d4fab03a525d7d562eb4f43412
JayCee-21 <nkezejennifer@gmail.com>
2025-06-27 21:36:01 +0100

    Add files via upload
    A userAPI commit

--- barrel.js
+++ barrel.js
@@ -1,4 +1,3 @@
-export { default as userRoute} from './userRoute.js'
-export { default as productRoute} from './productRoute.js'
-export { default as authRoute} from './authRoute.js'
-export { default as cartRoute} from './cartRoute.js'
+export * from './userSchema.js'
+export * from './productSchema.js'
+export * from './cartSchema.js'
